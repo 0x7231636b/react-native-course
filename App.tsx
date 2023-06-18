7f833d2737b870f7004a49d44aa49fb5b6c68ec5
Markus Hallmann <markus@hallmann.pw>
2023-06-18 11:31:34 +0200

    Added a MyButton Component as skeleton

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { MyButton } from "./components/MyButton";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello World!</Text>
+      <MyButton />
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/MyButton.tsx
@@ -0,0 +1,9 @@
+import { Button, View } from "react-native";
+
+export function MyButton() {
+  return (
+    <View>
+      <Button title="Press me" onPress={() => {}} />
+    </View>
+  );
+}
