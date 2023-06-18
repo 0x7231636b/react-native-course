74e809fff7757d3c3721fac87dbca96735bcf7fc
Markus Hallmann <markus@hallmann.pw>
2023-06-18 11:46:54 +0200

    Optional props

--- components/MyButton.tsx
+++ components/MyButton.tsx
@@ -1,13 +1,14 @@
 import { Button, View } from "react-native";
 
 interface MyButtonProps {
-  text: string;
+  text?: string;
+  onPress?: () => void;
 }
 
 export function MyButton(props: MyButtonProps) {
   return (
     <View>
-      <Button title={props.text} onPress={() => {}} />
+      <Button title={props.text} onPress={props.onPress} />
     </View>
   );
 }
